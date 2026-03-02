637bf6083af711aaed94addaa3b776e079eadbbc
RupaSiri <puppalarupasiri@gmail.com>
2026-03-02 20:03:13 +0530

    Restructured backend and updated quiz UI

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,11 +1,10 @@
-function loadQuestions() {
+ function loadQuestions() {
 
     fetch("http://localhost:8081/quiz/all")
     .then(response => response.json())
     .then(data => {
 
         let questionsDiv = document.getElementById("questions");
-
         questionsDiv.innerHTML = "";
 
         data.forEach(q => {
@@ -13,10 +12,28 @@ function loadQuestions() {
             let html = `
                 <div class="question-box">
                     <h3>${q.questionTitle}</h3>
-                    <p>1. ${q.option1}</p>
-                    <p>2. ${q.option2}</p>
-                    <p>3. ${q.option3}</p>
-                    <p>4. ${q.option4}</p>
+
+                    <label>
+                        <input type="radio" name="question${q.id}" value="${q.option1}">
+                        ${q.option1}
+                    </label><br>
+
+                    <label>
+                        <input type="radio" name="question${q.id}" value="${q.option2}">
+                        ${q.option2}
+                    </label><br>
+
+                    <label>
+                        <input type="radio" name="question${q.id}" value="${q.option3}">
+                        ${q.option3}
+                    </label><br>
+
+                    <label>
+                        <input type="radio" name="question${q.id}" value="${q.option4}">
+                        ${q.option4}
+                    </label><br>
+
+                    <input type="hidden" class="correct-answer" value="${q.correctAnswer}">
                 </div>
             `;
 
@@ -25,4 +42,24 @@ function loadQuestions() {
 
     })
     .catch(error => console.error(error));
+}
+function submitQuiz() {
+
+    let score = 0;
+
+    let questions = document.querySelectorAll(".question-box");
+
+    questions.forEach((q) => {
+
+        let selected = q.querySelector("input[type='radio']:checked");
+        let correct = q.querySelector(".correct-answer").value;
+
+        if (selected && selected.value === correct) {
+            score++;
+        }
+
+    });
+
+    document.getElementById("result").innerText =
+        "Your Score: " + score + " / " + questions.length;
 }
